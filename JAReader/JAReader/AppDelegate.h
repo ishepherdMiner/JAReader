f9fcfc9370a9d724284a1689cb66fbd70b20f93a
ishepherdMiner <iJason92@yahoo.com>
2017-06-15 22:10:37 +0800

    Init Project

---
+++ JAReader/JAReader/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  JAReader
+//
+//  Created by Jason on 15/06/2017.
+//  Copyright © 2017 Jason. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
